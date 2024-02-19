7e4ae32cd38b39c5e5703d3edfd6945303f0903a
Dylan Frankland <dfrankland@users.noreply.github.com>
2024-02-19 09:32:22 -0800

    add CLI for printing a sprite from the scraped menu home images

---
+++ cli-rs/build.rs
@@ -0,0 +1,43 @@
+use std::{env, fs, path::PathBuf};
+
+fn codegen(sprites: Vec<PathBuf>) -> String {
+    let inserts = sprites
+        .iter()
+        .map(|path| {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            let file_path = path.to_str().unwrap();
+            format!(r#"m.insert("{file_name}", include_bytes!("{file_path}").as_slice());"#)
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    format!(
+        r#"
+            use std::collections::HashMap;
+            use lazy_static::lazy_static;
+
+            lazy_static! {{
+                pub static ref SPRITES: HashMap<&'static str, &'static [u8]> = {{
+                    let mut m = HashMap::new();
+                    {inserts}
+                    m
+                }};
+            }}
+        "#
+    )
+}
+
+fn main() {
+    let files_path = fs::canonicalize("../data/files").unwrap();
+    let mut files = fs::read_dir(files_path).unwrap();
+    let mut sprites: Vec<PathBuf> = Vec::new();
+
+    while let Some(Ok(dir_entry)) = files.next() {
+        let file = dir_entry.path();
+        sprites.push(file.clone());
+        println!("cargo:rerun-if-changed={}", file.display());
+    }
+
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    fs::write(out_dir.join("files.rs"), codegen(sprites)).unwrap();
+}
---
+++ cli-rs/src/main.rs
@@ -0,0 +1,21 @@
+use image::{codecs::png::PngDecoder, DynamicImage};
+use rand::{thread_rng, Rng};
+use viuer::{print, Config};
+
+include!(concat!(env!("OUT_DIR"), "/files.rs"));
+
+fn main() {
+    let mut rng = thread_rng();
+    let index = rng.gen_range(0..SPRITES.len());
+    let sprite_bytes = SPRITES.values().skip(index).next().unwrap();
+    let sprite_image = DynamicImage::from_decoder(PngDecoder::new(*sprite_bytes).unwrap()).unwrap();
+    print(
+        &sprite_image,
+        &Config {
+            absolute_offset: false,
+            width: Some(20),
+            ..Default::default()
+        },
+    )
+    .unwrap();
+}
