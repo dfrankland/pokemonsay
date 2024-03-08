aececd075007f12f87b909d5c181ba29bfe88829
Dylan Frankland <dfrankland@users.noreply.github.com>
2024-03-08 15:09:25 -0700

    use quote macro to build CLI

--- cli-rs/build.rs
+++ cli-rs/build.rs
@@ -1,30 +1,36 @@
+use quote::{quote, ToTokens};
 use std::{env, fs, path::PathBuf};
 
 fn codegen(sprites: Vec<PathBuf>) -> String {
     let inserts = sprites
         .iter()
         .map(|path| {
-            let file_name = path.file_name().unwrap().to_str().unwrap();
-            let file_path = path.to_str().unwrap();
-            format!(r#"m.insert("{file_name}", include_bytes!("{file_path}").as_slice());"#)
+            let file_name = path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_token_stream();
+            let file_path = path.to_str().unwrap().to_token_stream();
+            quote! {
+                m.insert(#file_name, include_bytes!(#file_path).as_slice());
+            }
         })
-        .collect::<Vec<String>>()
-        .join("\n");
+        .collect::<Vec<_>>();
 
-    format!(
-        r#"
-            use std::collections::HashMap;
-            use lazy_static::lazy_static;
+    quote! {
+        use std::collections::HashMap;
+        use lazy_static::lazy_static;
 
-            lazy_static! {{
-                pub static ref SPRITES: HashMap<&'static str, &'static [u8]> = {{
-                    let mut m = HashMap::new();
-                    {inserts}
-                    m
-                }};
-            }}
-        "#
-    )
+        lazy_static! {
+            pub static ref SPRITES: HashMap<&'static str, &'static [u8]> = {
+                let mut m = HashMap::new();
+                #(#inserts)*
+                m
+            };
+        }
+    }
+    .to_string()
 }
 
 fn main() {
